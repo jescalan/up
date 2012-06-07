079374ae9c08f905364e106d513ea193d5f3c81d
idpro <kyle@idprojections.com>
2012-06-06 22:24:44 -0400

    helps if you add the correct functions

--- setup.sh
+++ setup.sh
@@ -1,5 +1,36 @@
 #!/bin/sh
 
+function ask {
+  while true; do
+ 
+    if [ "${2:-}" = "Y" ]; then
+      prompt="Y/n"
+      default=Y
+    elif [ "${2:-}" = "N" ]; then
+      prompt="y/N"
+      default=N
+    else
+      prompt="y/n"
+      default=
+    fi
+ 
+    # Ask the question
+    read -p "$1 [$prompt] " REPLY
+
+    # Default?
+    if [ -z "$REPLY" ]; then
+      REPLY=$default
+    fi
+ 
+    # Check if the reply is valid
+    case "$REPLY" in
+      Y*|y*) return 0 ;;
+      N*|n*) return 1 ;;
+    esac
+ 
+  done
+}
+
 database=''
 
 while getopts ":d:" opt; do
