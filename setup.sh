9e1a791330fab44135085dd1ae66b862b22f41de
Jeff Escalante <hello@jenius.me>
2012-09-11 13:14:22 -0400

    update ruby to latest version, update homebrew to latest version, smooth out handling of bash profile files, fix node.js install and update to latest version, add helpful bash aliases, don't install is user has rvm, add notes about support at the end

--- setup.sh
+++ setup.sh
@@ -31,6 +31,17 @@ function ask {
   done
 }
 
+# check to see if the user has already messed up their shit
+
+if [-z "$(which rvm)"]; then
+  echo "Looks like you installed RVM, which probably means your system is messed up"
+  echo "and/or you have been tinkering with your environement and ruby."
+  echo "email hello@jenius.me or talk to Jeff for further assistance"
+  exit 1
+fi
+
+# if not, let's make it happen!
+
 echo "-------------------------------------------------------------";
 echo "Please enter your password, we have to move some files around";
 echo "-------------------------------------------------------------";
@@ -41,16 +52,68 @@ echo "-----------------------------";
 echo "Setting up command line prefs";
 echo "-----------------------------";
 
-mv ~/.bash_profile ~/.bashrc;
-echo "PS1='\[\e[0;31m\]⚡\[\e[m\] \[\e[0;33m\]\${PWD##*/}\[\e[m\] '" >> ~/.bashrc;
-source ~/.bashrc;
+if ask "Do you want to pimp your terminal prompt?"; then
+
+  if [ -f ~/.bashrc ]; then
+    cat ~/.bashrc >> ~/.profile
+    rm ~/.bashrc
+    echo "I moved your settings from ~/.bashrc to ~/.profile";
+  fi
+
+  if [ -f ~/.bash_profile ]; then
+    cat ~/.bash_profile >> ~/.profile
+    rm ~/.bash_profile
+    echo "I moved your settings from ~/.bash_profile to ~/.profile";
+  fi
+
+  if [ -f ~/.bash_login ]; then
+    cat ~/.bash_login >> ~/.profile
+    rm ~/.bash_login
+    echo "I moved your settings from ~/.bash_login to ~/.profile";
+  fi
+
+  echo "PS1='\[\e[0;31m\]⚡\[\e[m\] \[\e[0;33m\]\${PWD##*/}\[\e[m\] '" >> ~/.profile;
+
+  if ask "Would you like some awesome command line shortcuts?"; then
+    echo 'alias ll="ls -lahG"' >> ~/.profile
+    echo 'alias reload="source ~/.profile"' >> ~/.profile
+    echo 'alias profile="vim ~/.profile"' >> ~/.profile
+    echo 'alias up="cd .."' >> ~/.profile
+    echo 'alias desktop="cd ~/Desktop"' >> ~/.profile
+    echo 'alias be="bundle exec"' >> ~/.profile
+    echo 'alias go="git checkout"' >> ~/.profile
+    echo 'alias godev="git checkout develop"' >> ~/.profile
+    echo 'alias s="echo \"\"; git status -sb; echo \"\""' >> ~/.profile
+    echo 'alias stage="git add ."' >> ~/.profile
+    echo 'alias c="fact git commit -am"' >> ~/.profile
+    echo 'alias got="git "' >> ~/.profile
+    echo 'alias get="git "' >> ~/.profile
+    echo 'alias glog="git log --graph --pretty=format:\"%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset\" --abbrev-commit --date=relative"' >> ~/.profile
+    echo 'alias branch="git branch"' >> ~/.profile
+    echo 'alias mpush="git push origin master"' >> ~/.profile
+    echo 'alias mpull="git pull origin master"' >> ~/.profile
+    echo 'alias dpush="git push origin develop"' >> ~/.profile
+    echo 'alias dpull="git pull origin develop"' >> ~/.profile
+    echo 'alias amend="git commit --amend -m"' >> ~/.profile
+    echo 'alias fact="echo \"-------------------------------------------------------------------------------\";  curl -s randomfunfacts.com | sed -n \"s/.*<i>\(.*\)<\/i>.*/\1/p\"; echo \"-------------------------------------------------------------------------------\";"' >> ~/.profile
+  fi
+
+  source ~/.profile;
+
+fi
 
 echo "-------------------";
 echo "Installing Homebrew";
 echo "-------------------";
 
-/usr/bin/ruby -e "$(/usr/bin/curl -fksSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)";
+ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go);
 brew update;
+
+echo "--------------";
+echo "Installing git";
+echo "--------------";
+
+brew install git;
 
 echo "----------------";
 echo "Installing rbenv";
@@ -71,15 +134,9 @@ echo "---------------------";
 echo "Installing ruby 1.9.3";
 echo "---------------------";
 
-rbenv install 1.9.3-p125;
+rbenv install 1.9.3-p194;
 rbenv rehash;
-rbenv global 1.9.3-p125;
-
-echo "--------------";
-echo "Installing git";
-echo "--------------";
-
-brew install git;
+rbenv global 1.9.3-p194;
 
 echo "-----------------";
 echo "Updating rubygems";
@@ -127,21 +184,26 @@ echo "----------------";
 
 sudo gem install rails;
 
-echo "------------------";
-echo "Installing Node.js";
-echo "------------------";
-if ask "Do you want to install Node.js?"; then
-  brew install node
-  
-  if ask "Do you want to install NPM?"; then
-    curl http://npmjs.org/install.sh | sh
+if ask "Do you want to install Node.js? (server side javascript development)"; then
+
+  echo "Downloading..."
+  NODEVER=0.8.8
+  curl -O http://nodejs.org/dist/v$NODEVER/node-v$NODEVER.pkg
+  echo "Installing..."
+  sudo installer -pkg node-v$NODEVER.pkg -target /
+
+  if ask "Would you like to install some important node packages?"
+    echo "Installing bower (client-side javascript package management)"
+    npm install bower -g
+    echo "Installing coffeescript (javascript except a million times better)"
+    npm install coffee-script -g
+    echo "Installing supervisor (node app auto-reload on changes)"
+    npm install supervisor -g
+    echo "Installing express (web framework for node)"
+    npm install express -g
   fi
 
-  if ask "Do you want to install ender?"; then
-    npm install ender -g
-  fi
 fi
-
 
 echo "----------------";
 echo "Database Options";
@@ -179,9 +241,9 @@ if ask "Would you like to install databases? (optional)" N; then
     fi  
   fi
 
-  if ask "Do you want to install Redis.IO?" Y; then
+  if ask "Do you want to install Redis?" Y; then
     echo "-------------------";
-    echo "Installing Redis.IO";
+    echo "Installing Redis";
     echo "-------------------";
     brew install redis;
 
@@ -214,4 +276,6 @@ fi
 
 echo "---------------------------------------------------------------------"
 echo "Boom! All finished. Everything should be good to go."
+echo "If there were any errors, save the output where the error came up"
+echo "and send it to hello@jenius.me for help!"
 echo "---------------------------------------------------------------------"
